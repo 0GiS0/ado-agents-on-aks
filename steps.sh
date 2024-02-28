a1564e255ceea42b47640ff0394274d2fff45b25
0GiS0 <giselatb@outlook.com>
2024-02-28 22:43:02 +0100

    Add Dockerfile, Azure Pipelines configuration, deployment files, and necessary dependencies

--- steps.sh
+++ steps.sh
@@ -2,26 +2,140 @@
 RESOURCE_GROUP="ado-agents-on-aks"
 AKS_NAME="ado-agents-cluster"
 LOCATION="uksouth"
+ACR_NAME="adoimages"
 
 # Create a resource group
 az group create \
 --name $RESOURCE_GROUP \
 --location $LOCATION
 
+# Create Azure Container Registry
+az acr create \
+--resource-group $RESOURCE_GROUP \
+--name $ACR_NAME \
+--sku Basic
+
 # Create AKS cluster with KEDA enabled
 az aks create \
 --resource-group $RESOURCE_GROUP \
 --name $AKS_NAME \
 --enable-keda \
---generate-ssh-keys
+--generate-ssh-keys \
+--attach-acr $ACR_NAME
 
 # Get the credentials for the AKS cluster
 az aks get-credentials \
 --resource-group $RESOURCE_GROUP \
---name $AKS_NAME
+--name $AKS_NAME \
+--overwrite-existing
 
-# Install Azure DevOps extension for Azure CLI
-az extension add --name azure-devops
+# Create an agent pool
+# Organization Settings > Agent Pools > Add Pool > Self Hosted > Name: agents-on-aks
+AGENT_POOL_NAME="agents-on-aks"
+ORGANIZATION_NAME="returngisorg"
 
-# Create an agent pool for the AKS cluster
-az pipelines pool -h
+AGENT_POOL_ID=$(az pipelines pool list \
+--organization https://dev.azure.com/$ORGANIZATION_NAME \
+--query "[?name=='$AGENT_POOL_NAME'].id" --output tsv)
+
+echo "Agent Pool ID: $AGENT_POOL_ID"
+
+# Generate a PAT in Azure DevOps
+# It is necessary to have an Azure Devops token to be able to register our agents in the agent pool.
+# https://dev.azure.com/returngisorg/_usersSettings/tokens
+source .env
+
+# Save the PAT in a Kubernetes secret
+# kubectl create secret generic ado-pat --from-literal=pat=$PAT
+
+# Generate docker image with ACR
+# The Dockerfile is in the same directory as this script
+az acr build \
+--resource-group $RESOURCE_GROUP \
+--registry $ACR_NAME \
+--image ado-agent:latest .
+
+# Create a Kubernetes deployment
+# cat <<EOF > deployment.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: azdevops
+data:
+  AZP_TOKEN: '$(echo $PAT | base64)'
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: azdevops-deployment
+  labels:
+    app: azdevops-agent
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: azdevops-agent
+  template:
+    metadata:
+      labels:
+        app: azdevops-agent
+    spec:
+      containers:
+      - name: azdevops-agent
+        image: $ACR_NAME.azurecr.io/ado-agent:latest
+        env:
+          - name: AZP_URL
+            value: "https://dev.azure.com/$ORGANIZATION_NAME" 
+          - name: AZP_POOL
+            value: "$AGENT_POOL_NAME"
+          - name: AZP_TOKEN
+            valueFrom:
+              secretKeyRef:
+                name: azdevops
+                key: AZP_TOKEN
+EOF
+
+# Check the status of the deployment
+watch kubectl get pods
+kubectl logs -f $(kubectl get pods -l app=azdevops-agent -o jsonpath="{.items[0].metadata.name}")
+
+# Let create KEDA configuration to scale the agents
+# cat <<EOF > keda-config.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: pipeline-auth
+data:
+  personalAccessToken: '$(echo $PAT | base64)'
+---
+apiVersion: keda.sh/v1alpha1
+kind: TriggerAuthentication
+metadata:
+  name: pipeline-trigger-auth
+spec:
+  secretTargetRef:
+    - parameter: personalAccessToken
+      name: pipeline-auth
+      key: personalAccessToken
+---
+apiVersion: keda.sh/v1alpha1
+kind: ScaledObject
+metadata:
+  name: azure-pipelines-scaledobject
+spec:
+  scaleTargetRef:
+    name: azdevops-deployment
+  minReplicaCount: 1
+  maxReplicaCount: 5 #Maximum number of parallel instances
+  triggers:
+  - type: azure-pipelines
+    metadata:
+      poolID: "$AGENT_POOL_ID"
+      organizationURLFromEnv: "AZP_URL"
+    authenticationRef:
+     name: pipeline-trigger-auth
+EOF
+
+kubectl get scaledobject
